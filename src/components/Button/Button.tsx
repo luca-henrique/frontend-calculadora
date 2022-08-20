2e40ddb9fe2f5dc76f387a0692a7897745380690
Lucas Henrique <lukas.paes18@gmail.com>
2022-08-20 19:40:32 -0300

    feature:ajustando button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,18 @@
+import React, {ReactElement} from 'react';
 import './Button.css';
 
 interface ButtonProps {
   label?: string;
+  type?: string;
+  handleClick?: any;
 }
 
-const Button = ({label}: ButtonProps) => {
-  return <button className='button'>{label}</button>;
+const Button = ({label, type, handleClick}: ButtonProps) => {
+  return (
+    <button onClick={() => handleClick(label)} className={`button ${type}`}>
+      {label}
+    </button>
+  );
 };
 
 export default Button;
--- src/pages/Main/Calculator.tsx
+++ src/pages/Main/Calculator.tsx
@@ -2,28 +2,39 @@ import './calculator.css';
 
 import Button from '../../components/Button/Button';
 import Display from '../../components/Display/Display';
+import {useState} from 'react';
 
 const Calculator = () => {
+  const [total, setTotal] = useState(0);
+
+  const clearMemory = () => {
+    setTotal(0);
+  };
+
+  const setOperation = () => {};
+
+  const addDigit = () => {};
+
   return (
     <div className='calculator'>
-      <Display value={100} />
-      <Button label='AC' />
-      <Button label='/' />
-      <Button label='7' />
-      <Button label='8' />
-      <Button label='9' />
-      <Button label='*' />
-      <Button label='4' />
-      <Button label='5' />
-      <Button label='6' />
-      <Button label='-' />
-      <Button label='1' />
-      <Button label='2' />
-      <Button label='3' />
-      <Button label='+' />
-      <Button label='0' />
-      <Button label='.' />
-      <Button label='=' />
+      <Display value={total} />
+      <Button label='AC' type='triple' handleClick={clearMemory} />
+      <Button label='/' handleClick={setOperation} type='operation' />
+      <Button label='7' handleClick={addDigit} />
+      <Button label='8' handleClick={addDigit} />
+      <Button label='9' handleClick={addDigit} />
+      <Button label='*' handleClick={setOperation} type='operation' />
+      <Button label='4' handleClick={addDigit} />
+      <Button label='5' handleClick={addDigit} />
+      <Button label='6' handleClick={addDigit} />
+      <Button label='-' handleClick={setOperation} type='operation' />
+      <Button label='1' handleClick={addDigit} />
+      <Button label='2' handleClick={addDigit} />
+      <Button label='3' handleClick={addDigit} />
+      <Button label='+' handleClick={setOperation} type='operation' />
+      <Button label='0' handleClick={addDigit} type='double' />
+      <Button label='.' handleClick={addDigit} />
+      <Button label='=' handleClick={setOperation} type='operation' />
     </div>
   );
 };
